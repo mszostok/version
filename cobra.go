fdb28f474b8ffd1873dc5f58560d6355ca37bcf7
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-07-22 23:26:46 +0200

    Add cobra support

---
+++ cobra.go
@@ -0,0 +1,36 @@
+package version
+
+import (
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// TODO: func options to inform if server version
+// TODO: to add template?
+
+var example = `
+  <cli> version
+  <cli> version -o=json
+  <cli> version -o=yaml
+  <cli> version -o=pretty
+  <cli> version -o=short
+`
+
+// NewCobraCmd returns a root cobra.Command for printing CLI version.
+func NewCobraCmd(name string) *cobra.Command {
+	printer := NewPrinter()
+	CollectFromBuildInfo()
+
+	ver := &cobra.Command{
+		Use:     "version",
+		Short:   "Print the CLI version",
+		Example: strings.ReplaceAll(example, "<cli>", name),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return printer.Print(cmd.OutOrStdout(), Get())
+		},
+	}
+
+	printer.RegisterFlags(ver.Flags())
+	return ver
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/mszostok/version
 
 go 1.18
+
+require (
+	github.com/gookit/color v1.5.1
+	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f
+	github.com/spf13/cobra v1.5.0
+	github.com/spf13/pflag v1.0.5
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
