75daa2c9bbe4174dc29dd1dbfce050fca4c4f409
Mateusz Szostok <szostok.mateusz@gmail.com>
2022-09-04 21:40:03 +0200

    Add aliases for Cobra version command (#25)

---
+++ examples/cobra-alias/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"go.szostok.io/version/extension"
+)
+
+// NewRoot returns a root cobra.Command for the whole CLI.
+func NewRoot() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "example",
+		Short: "An example CLI built with github.com/spf13/cobra",
+	}
+
+	cmd.AddCommand(
+		// you just need to add this, and you are done.
+		extension.NewVersionCobraCmd(
+			extension.WithAliasesOptions("v"),
+		),
+	)
+
+	return cmd
+}
+
+func main() {
+	if err := NewRoot().Execute(); err != nil {
+		os.Exit(1)
+	}
+}
--- extension/opts.go
+++ extension/opts.go
@@ -9,7 +9,7 @@ import (
 // It allows having the same functional opt func across all constructors. For example:
 //
 // - extension.NewVersionCobraCmd(extension.WithUpgradeNotice(..))
-// - extension.NewVersionCobraCmd(extension.WithUpgradeNotice(..))
+// - extension.NewVersionCobraCmd(extension.WithPrinterOptions(..))
 
 type (
 	// CobraOption provides an option to set a Cobra options.
@@ -21,24 +21,25 @@ type (
 	// CobraOptions holds Cobra command possible customization settings.
 	CobraOptions struct {
 		PrinterOptions []printer.ContainerOption
+		Aliases        []string
 	}
 )
 
 // CustomPrinterOptions provides an option to set a custom printer related options across multiple constructors.
 type CustomPrinterOptions struct {
-	PrinterOptions []printer.ContainerOption
+	printerOptions []printer.ContainerOption
 }
 
 // WithPrinterOptions sets a custom printer related options.
 func WithPrinterOptions(opts ...printer.ContainerOption) *CustomPrinterOptions {
 	return &CustomPrinterOptions{
-		PrinterOptions: opts,
+		printerOptions: opts,
 	}
 }
 
 // ApplyToCobraOption sets a given option for Cobra.
 func (c *CustomPrinterOptions) ApplyToCobraOption(options *CobraOptions) {
-	options.PrinterOptions = append(options.PrinterOptions, c.PrinterOptions...)
+	options.PrinterOptions = append(options.PrinterOptions, c.printerOptions...)
 }
 
 // EnableUpgradeNotice provides an option to enable upgrade notice across multiple constructors.
@@ -59,15 +60,35 @@ func WithUpgradeNotice(owner, repo string, opts ...upgrade.Options) *EnableUpgra
 
 // ApplyToCobraOption sets a given option for Cobra.
 // It's a syntax sugar for:
-// 	extension.NewVersionCobraCmd(
+//
+//	extension.NewVersionCobraCmd(
 //		extension.WithPrinterOptions(
 //			printer.WithUpgradeNotice("mszostok", "codeowners-validator"),
 //		),
 //	)
+//
 // so you can just do:
 // extension.NewVersionCobraCmd(
+//
 //		extension.WithUpgradeNotice("mszostok", "codeowners-validator"),
 //	)
 func (c *EnableUpgradeNotice) ApplyToCobraOption(options *CobraOptions) {
 	options.PrinterOptions = append(options.PrinterOptions, printer.WithUpgradeNotice(c.owner, c.repo, c.upgradeOpts...))
+}
+
+// AliasesOptions provides an option to set a custom printer related options across multiple constructors.
+type AliasesOptions struct {
+	aliases []string
+}
+
+// WithAliasesOptions sets a given aliases for the 'version' command.
+func WithAliasesOptions(aliases ...string) *AliasesOptions {
+	return &AliasesOptions{
+		aliases: aliases,
+	}
+}
+
+// ApplyToCobraOption sets a given option for Cobra.
+func (c *AliasesOptions) ApplyToCobraOption(options *CobraOptions) {
+	options.Aliases = c.aliases
 }
